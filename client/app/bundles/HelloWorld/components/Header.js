b6dd4f419236125eab61feb73f50bc6d5ea4820f
maximHome <maxstbn@gmail.com>
2016-08-17 22:39:59 +0300

    сделал header

--- client/app/bundles/HelloWorld/components/Header.js
+++ client/app/bundles/HelloWorld/components/Header.js
@@ -7,56 +7,77 @@ import withScroll from '../../decorators/withViewportScroll';
 
 @withScroll
 class Header extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    let scrollTop = this.props.viewportScroll.top;
-    if (scrollTop > 100 && scrollTop < 500) {
-      if (this.props.viewportScroll.scrollDown) {
-        var scrollHead = 'b-header__hideDown';
-      }
-      if (this.props.viewportScroll.scrollUp) {
-        var scrollHead = 'b-header__hideUp';
-      }
+    constructor(props) {
+        super(props);
     }
-    if (scrollTop > 500) {
-      if (this.props.viewportScroll.scrollDown) {
-        var scrollHead = 'b-header__ScrollDown';
-      }
-      if (this.props.viewportScroll.scrollUp) {
-        var scrollHead = 'b-header__ScrollUp';
-      }
+    
+    render() {
+        let scrollTop = this.props.viewportScroll.top;
+        if (scrollTop > 100 && scrollTop < 500) {
+            if (this.props.viewportScroll.scrollDown) {
+                var scrollHead = 'b-header__hideDown';
+            }
+            if (this.props.viewportScroll.scrollUp) {
+                var scrollHead = 'b-header__hideUp';
+            }
+        }
+        if (scrollTop > 500) {
+            if (this.props.viewportScroll.scrollDown) {
+                var scrollHead = 'b-header__ScrollDown';
+            }
+            if (this.props.viewportScroll.scrollUp) {
+                var scrollHead = 'b-header__ScrollUp';
+            }
+        }
+        
+        return (
+            <header className={`container__Header`}>
+                <div className="Header">
+                    
+                    <div className="Header__logo">
+                        <a className="Logo"
+                           href="/"></a>
+                    </div>
+                    
+                    
+                    <div className="Header__nav-container">
+                        <div className="nav">
+                            <div className=""
+                                 role="navigation">
+                                <div className="b-nav"
+                                     role="navigation">
+                                    <a className="b-nav__link"
+                                       href="/about">О компании</a>
+                                    <a className="b-nav__link"
+                                       href="/contact">Контакты</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Header__info-container">
+                        <div className="info">
+                            
+                            <div className="info__logo">
+                                <a className="Logo"
+                                   href="/"></a>
+                            </div>
+                            
+                            <div className="info_tel-login">
+                                <div className="tel">
+                                    <span className="tel__icon icon-emb-phone"></span>
+                                    <div className="tel__number-time">
+                                        <div className="number">+7 495 742-1212</div>
+                                        <div className="time">круглосуточно</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        );
     }
-
-    return (
-      <header className="header">
-        <div className={`b-header__container ${scrollHead}`}>
-          <div className="b-header">
-            {/*<NavMobile className="b-header__nav-mobile"/>*/}
-            {/*<Logo className="b-header__logo"/>*/}
-  
-            <div className="b-header__logo">
-              <a className="Logo" href="/"></a>
-            </div>
-            
-            <div className="b-header__nav" role="navigation">
-              <div className="b-nav" role="navigation">
-                <span className="b-nav__text">+7 495 742-1212</span>
-                <a className="b-nav__link" href="/about">О компании</a>
-                <a className="b-nav__link" href="/contact">Контакты</a>
-                <a className="b-nav__help b-help" href="https://inna.ru/#/help" title="Помощь" target="_blank">
-                  <i className="b-help__icon" title="Помощь" alt="Помощь"></i>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-    );
-  }
-
+    
 }
 
 export default Header;
--- client/app/bundles/HelloWorld/startup/serverRegistration.jsx
+++ client/app/bundles/HelloWorld/startup/serverRegistration.jsx
@@ -1,4 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 import HelloWorldApp from './HelloWorldAppServer';
+import Header from '../components/Header';
 
 ReactOnRails.register({ HelloWorldApp });
+ReactOnRails.register({ Header });
