2e6c2053d9ff0efc7f03020cde03e3c3e7a8be1d
maximHome <maxstbn@gmail.com>
2016-08-25 14:31:48 +0300

    фиксы

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#index'
 
-  post '/updateTours' => 'pages#update'
+  post '/pages/updateTours' => 'pages#update'
   resources :pages, only: [:index, :show]
 
   get 'hello_world', to: 'hello_world#index'
